4e9a3f629739272d1617b1d14475d1179433c3cd
FinnJakob <finnjakobe@gmail.com>
2023-03-28 10:53:44 +0200

    Create MovieList.js

---
+++ my-app/src/MovieList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function MovieList(props) {
+    const { movies } = props;
+
+    return (
+        <div className='PosterCard'>
+            {movies.map(movie => (
+                <article key={movie.imdbID}>
+                    <img src={movie.Poster} alt={movie.Title} />
+                    <div>
+                        <h3>{movie.Title}</h3>
+                        <p className='CardYear'>Year: {movie.Year}</p>
+                        <p className='CardType'>Type: {movie.Type}</p>
+                    </div>
+                </article>
+            ))}
+        </div>
+    );
+}
